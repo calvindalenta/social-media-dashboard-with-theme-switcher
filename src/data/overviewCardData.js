2c3645a3dc024e181c44db9fc8e811ea1ee7b42c
Calvin Dalenta <calvindalentark@gmail.com>
2021-09-28 02:53:18 +0700

    Add overview card data

---
+++ src/data/overviewCardData.js
@@ -0,0 +1,74 @@
+const overviewCardData = [
+    {
+        platform: 'facebook',
+        type: 'Page Views',
+        number: 87,
+        trend: {
+            type: 'up',
+            text: '3%'
+        } 
+    },
+    {
+        platform: 'facebook',
+        type: 'Likes',
+        number: 52,
+        trend: {
+            type: 'down',
+            text: '2%'
+        } 
+    },
+    {
+        platform: 'instagram',
+        type: 'Likes',
+        number: 5462,
+        trend: {
+            type: 'up',
+            text: '2257%'
+        } 
+    },
+    {
+        platform: 'instagram',
+        type: 'Profile Views',
+        number: 52000,
+        trend: {
+            type: 'up',
+            text: '1375%'
+        } 
+    },
+    {
+        platform: 'twitter',
+        type: 'Retweets',
+        number: 117,
+        trend: {
+            type: 'up',
+            text: '303%'
+        } 
+    },
+    {
+        platform: 'twitter',
+        type: 'Likes',
+        number: 507,
+        trend: {
+            type: 'up',
+            text: '553%'
+        } 
+    },
+    {
+        platform: 'youtube',
+        type: 'Likes',
+        number: 107,
+        trend: {
+            type: 'down',
+            text: '19%'
+        } 
+    },
+    {
+        platform: 'youtube',
+        type: 'Total Views',
+        number: 1407,
+        trend: {
+            type: 'down',
+            text: '12%'
+        } 
+    },
+];
