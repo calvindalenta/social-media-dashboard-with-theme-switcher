03d8aae65f510645bb5e44353fd7224ab9baba2b
Calvin Dalenta <calvindalentark@gmail.com>
2021-09-28 01:57:40 +0700

    Add props to SocialCard

--- src/components/SocialCard.js
+++ src/components/SocialCard.js
@@ -1,24 +1,48 @@
+import { objectOf, number, string } from 'prop-types';
+
 import MediumCard from "./styled/MediumCard";
 import Account from "./styled/Account";
 import Followers from "./styled/Followers";
 import Trend from "./styled/Trend";
 
-export default function SocialCard(){
+export default function SocialCard({socialIcon, userName, followers, trend}){
+
+    const trendIcon = trend.type === 'up' ? "images/icon-up.svg" : "images/icon-down.svg"
 
     return (
         <MediumCard>
             <Account>
-                <img src="images/icon-facebook.svg" alt="Icon"/>
-                <p>@nathanf</p>
+                <img src={socialIcon} alt="Icon"/>
+                <p>{userName}</p>
             </Account>
             <Followers>
-                <h1>1987</h1>
+                <h1>{followers}</h1>
                 <p>Followers</p>
             </Followers>
             <Trend>
-                <img src="images/icon-up.svg" alt="Trend Icon" />
-                <p>1099 Today</p>
+                <img src={trendIcon} alt="Trend Icon" />
+                <p>{trend.text}</p>
             </Trend>
         </MediumCard>
     );
+}
+
+SocialCard.propTypes = {
+    socialIcon: string.isRequired,
+    userName: string.isRequired,
+    followers: number.isRequired,
+    trend: objectOf(
+        string.isRequired,
+        string.isRequired
+    ).isRequired
+}
+
+SocialCard.defaultProps = {
+    socialIcon: 'images/icon-facebook.svg',
+    userName: '-',
+    followers: 0,
+    trend: {
+        type: 'up',
+        text: '-'
+    }
 }
--- src/components/styled/Container.js
+++ src/components/styled/Container.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-    width: 80%;
+    width: 90%;
     margin: auto;
 
     border: 1px solid white;
